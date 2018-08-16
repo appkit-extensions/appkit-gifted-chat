0c74b7a3a96abaa7f83b399982b6af260cef1e10
Dave Weaver <dave@markkup.com>
2018-08-16 00:00:51 -0400

    exported internals

--- GiftedChat.js
+++ GiftedChat.js
@@ -1,2 +1,18 @@
-import { GiftedChat }  from '@appkit-shared/react-native-gifted-chat'
+import {
+    GiftedChat,
+    Avatar,
+    Day,
+    MessageText,
+    MessageImage,
+    Time,
+    utils
+}  from '@appkit-shared/react-native-gifted-chat'
+
+GiftedChat.Avatar = Avatar;
+GiftedChat.Day = Day;
+GiftedChat.MessageText = MessageText;
+GiftedChat.MessageImage = MessageImage;
+GiftedChat.Time = Time;
+GiftedChat.utils = utils;
+
 export default GiftedChat
